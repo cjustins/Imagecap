870c12be97e13a90ba9cc90ff692fef07fafb546
cjustins <146534362+cjustins@users.noreply.github.com>
2023-09-30 14:13:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ model= load_model('image_cap_model.h5')
 def predict_image(image):
     model = VGG16()
     # load an image from file
-    image = load_img(image, target_size=(224, 224))
+    #image = load_img(image, target_size=(224, 224))
     # convert the image pixels to a numpy array
-    image = img_to_array(image)
+    #image = img_to_array(image)
     # reshape data for the model
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
     # prepare the image for the VGG model
